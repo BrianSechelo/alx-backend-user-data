c9b524a9bd60447fef306261e5b6d51c97a8b41c
BrianSechelo <sechbrian123@gmail.com>
2024-09-03 20:55:48 +0000

    Task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,3 +53,27 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded Base64 string.
+
+        Returns:
+            tuple: (user_email, user_password) or (None, None) if conditions are not met.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string into user email and password
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password
+
