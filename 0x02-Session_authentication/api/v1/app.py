9b14cc0d1170a6f750328891d2d825cbd765d99e
BrianSechelo <sechbrian123@gmail.com>
2024-09-05 12:48:32 +0000

    Add Task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,23 +4,27 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, abort
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth  # Import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-
 auth = None
-if os.getenv("AUTH_TYPE") == "basic_auth":
-    auth = BasicAuth()
-elif os.getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
 
+auth_type = os.getenv("AUTH_TYPE")  # Get the value of AUTH_TYPE
+
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "session_auth":  # If AUTH_TYPE is session_auth, use SessionAuth
+    auth = SessionAuth()
+elif auth_type == "auth":
+    auth = Auth()
 
 @app.before_request
 def before_request_func():
@@ -36,28 +40,23 @@ def before_request_func():
     user = auth.current_user(request)
     if user is None:
         abort(403)
-    
-    # Assign the current user to request.current_user
-    request.current_user = user
 
+    request.current_user = user
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """doc doc Unauthorized handler"""
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
-
 
 @app.errorhandler(403)
 def forbidden(error):
-    """Error handler for 403 Forbidden"""
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for Session Authentication."""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """Session authentication class that inherits from Auth."""
+    pass
