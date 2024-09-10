e9f91cc1fc642118c1775fa342233e21d5b1fa79
BrianSechelo <sechbrian123@gmail.com>
2024-09-10 23:21:08 +0000

    Task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -41,13 +42,23 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        """
-        for key in kwargs.keys():
-            if not hasattr(User, key):
-                raise InvalidRequestError()
+        """doc doc doc"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+        return user
 
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user:
-            return user
-        raise NoResultFound()
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """doc doc doc"""
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError
+
+        self._session.commit()
